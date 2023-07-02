caac8c526085752716638be5b7763a37ab6c9783
nickson35 <nicksonpolouse342@gmail.com>
2023-07-02 21:18:29 +0530

    host api live

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  getAllProducts,
-  getAllProductsTesting,
-} = require("../controllers/products");
+const { getAllProducts } = require("../controllers/products");
 
 router.route("/").get(getAllProducts);
 
