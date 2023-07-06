eb80ea13c4ff92960f64d2f8789822c5319b09d4
nickson35 <nicksonpolouse342@gmail.com>
2023-07-07 00:13:03 +0530

    host live

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const connectDB = require("./database/connect");
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const products_routes = require("./routes/products");
 
@@ -17,8 +17,8 @@ app.use("/api/products", products_routes);
 const start = async () => {
   try {
     await connectDB(process.env.MONGODB_URL);
-    app.listen(PORT, () => {
-      console.log(`${PORT} Yes I am connected`);
+    app.listen(port, () => {
+      console.log(`${port} Yes I am connected`);
     });
   } catch (error) {
     console.log(error);
