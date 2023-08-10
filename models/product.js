cbef3cf71d590b825e9ddab13ad98cead2573fed
nickson35 <nicksonpolouse342@gmail.com>
2023-08-10 19:26:53 +0530

    work please

--- models/product.js
+++ models/product.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
+const imageSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: true,
+  },
+});
+
 const productScheme = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
   title: { type: String },
-  img: [
-    {
-      url: { type: String, required: true },
-    }
-  ],
+  img: { type: [imageSchema], required: true },
   description: { type: String },
   size: { type: [String], required: true },
   price: { type: Number },
